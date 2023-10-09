8f54904072340fa3db59c00fff84348024fe9d73
Nkemjika1divine <cndivine@gmail.com>
2023-10-09 09:54:16 -0700

    Airbnb clone console

--- models/base_model.py
+++ models/base_model.py
@@ -2,3 +2,23 @@
 """This module houses the BaseModel class"""
 from uuid import uuid4
 from cmd import Cmd
+from datetime import datetime
+
+
+class BaseModel:
+    """This is the Base class for all the packages"""
+    self.id = str(uuid4())
+    created_at = datetime.now()
+    updated_at = datetime.now()
+
+    def save(self):
+        """This updates updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing keys/values of __dict__ of inst"""
+
+
+    def __str__(self):
+        """This is a string representation of the basemodel class"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
