0cdcd955104003b496b7ddf248c334d113081ec5
Nkemjika1divine <cndivine@gmail.com>
2023-10-10 09:01:50 -0700

    Airbnb clone console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,15 +31,21 @@ class FileStorage:
             json.dump(obj, f)
 
     def reload(self):
-        "This deserializes json strings from files into __objects"""
+        """This deserializes json strings from files into __objects"""
         if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r") as f:
                 try:
                     obj = json.load(f)
+                    # parse through the files
                     for key, value in obj.items():
+                        # split key into classname and id so you can use key
                         name_of_class, obj_id = key.split('.')
+                        # evaluate the class name to get a reference to it
                         cls = eval(name_of_class)
+                        # assign keyword arguments using the values
+                        # tbis uses the cls instane to achieve that
                         new_obj = cls(**value)
+                        # now add the new object to the __objects dictionary
                         self.new(new_obj)
                 except Exception as E:
                     pass
